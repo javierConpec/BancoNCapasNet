69bb6b6e317a2d17b040e55208777a75b5cae6b3
unknown <javierconpec@gmail.com>
2023-02-22 14:18:50 -0500

    Se agrego las entidades Cliente, TipoCliente, TipoDocumento

---
+++ Banco.Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Entidades
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string RazonSocial { get; set; }
+        public string NumeroDocumento { get; set; }
+        public int IDTipoCliente { get; set; }
+        public int IDTipodocumento { get; set; }
+        public string Direccion { get; set; }
+        public string Referencias { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Banco.Entidades/TipoDocumento.cs
+++ Banco.Entidades/TipoDocumento.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Banco.Entidades
 {
-    public class Class1
+    public class TipoDocumento
     {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
     }
 }
